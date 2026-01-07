d17759265bd232f1c84acd1a0b2a5b9b8e2b0653
Tutul <tutulkabir@gmail.com>
2026-01-08 00:17:01 +0530

    Starter pack setup completed

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import express, {
+  type Application,
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { envVars } from "./app/config/env";
+import { router } from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+//parser
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", router);
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Server is running..",
+    environment: envVars.NODE_ENV,
+    uptime: process.uptime().toFixed(2) + " sec",
+    timeStamp: new Date().toISOString(),
+  });
+});
+
+app.use(globalErrorHandler);
+
+// app.use(notFound);
+
+export default app;
